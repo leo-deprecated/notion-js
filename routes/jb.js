efa94382f4e6ed699d93f949adfca7bbcda14574
Leo-mineis <leo@mineis.io>
2022-11-27 16:52:40 +0900

    feat: connect maria db in rds

--- routes/jb.js
+++ routes/jb.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql      = require('mysql');
+var conn = mysql.createConnection({
+  host     : '',
+  user     : '',
+  password : ''
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('I am Jubin!');
+  conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  conn.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    console.log(rows);
+    if (rows[0].solution == 2) {
+      res.send('Solution is 2');
+    } else {
+      res.send('Solution is not 2');
+    };
+  });
+
+  conn.end();
+
 });
 
 module.exports = router;
